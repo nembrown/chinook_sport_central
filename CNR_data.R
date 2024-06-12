801d64b0c85bfe1eb72b0313dcb85f62fd61a5a3
Heidi Van Vliet <heidi.vanvliet@dfo-mpo.gc.ca>
2024-06-12 11:26:40 -0700

    added roll up script for ERA-level fisheries

--- CNR_data.R
+++ CNR_data.R
@@ -9,3 +9,16 @@ source("final_model.R")
 #catch_estimate_predicted
 
 View(Season_south_combined)
+
+#roll up to ERA fishery level for CNR file
+#does not include NBC AABM S, NBC ISBM S or CBC S
+CNR_roll_up_south <- Season_south_combined %>%
+  mutate(finescale_fishery_old = str_replace_all(finescale_fishery_old,
+                                                 c("NGS" = "GS",
+                                                   "SGS" = "GS",
+                                                   "NWCVI S AABM" = "WCVI S AABM",
+                                                   "SWCVI S AABM" = "WCVI S AABM",
+                                                   "NWCVI S ISBM" = "WCVI S ISBM",
+                                                   "SWCVI S ISBM" = "WCVI S ISBM"))) %>%
+  group_by(YEAR, finescale_fishery_old, kept_status) %>%
+  summarize(total = sum(catch_estimate_predicted))
