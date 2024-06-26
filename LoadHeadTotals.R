fa9cabed7faef51222210ef0e6eb3f113daee867
Nicholas Komick <nicholas.komick@dfo-mpo.gc.ca>
2024-06-26 15:38:10 -0700

    improve the analysis of heads

--- LoadHeadTotals.R
+++ LoadHeadTotals.R
@@ -23,26 +23,6 @@ LoadHeadTotals <- function() {
 }
 
 
-head_df <-
-  LoadHeadTotals() |>
-  filter(!is.na(region))
-
-
-head_catch_df <-
-  Season_south_combined |>
-  filter(status == "marked_Kept_total") |>
-  full_join(head_df, c(YEAR="year", finescale_fishery = "region")) |>
-  mutate(YEAR = as.integer(YEAR),
-         submit_rate = head_total/catch_estimate_predicted)
-
-
-head_catch_model_df <-
-  head_catch_df |>
-  filter(catch_estimate_predicted > 0, !(grepl("NBC", head_catch_df$finescale_fishery) & YEAR < 2015))
-
-head_model <- lm(head_total ~ catch_estimate_predicted + 0, data=head_catch_model_df)
-
-
 lm_eqn <- function(df){
   m <- lm(head_total ~ catch_estimate_predicted + 0, df)
 
@@ -53,36 +33,59 @@ lm_eqn <- function(df){
   as.character(as.expression(eq));
 }
 
-ggplot(data = head_catch_model_df, aes(x=catch_estimate_predicted,
-                                       y=head_total)) +
-  geom_point(aes(color = finescale_fishery),
-             size=2) +
-  geom_smooth(method='lm', formula= y~x) +
-  geom_text(x = 500, y = 5000, label = lm_eqn(head_catch_model_df), parse = TRUE, hjust=0, vjust=0)
-
-head_catch_model_df |>
-  filter(catch_estimate_predicted > 20) |>
-  ggplot(aes(x=YEAR, y=submit_rate, color=finescale_fishery)) +
-  geom_line(size = 3)
-
-region_head_df <-
-  head_catch_model_df |>
-  mutate(region_name = substring(finescale_fishery,1,6)) |>
-  mutate(region_name = paste0(region_name,
-                              if_else(grepl("ISBM", finescale_fishery), "ISBM", ""))) |>
-  mutate(region_name = trimws(region_name)) |>
-  group_by(YEAR, region_name) |>
-  summarize(head_total = sum(head_total, na.rm=TRUE),
-            catch_estimate_predicted = sum(catch_estimate_predicted, na.rm=TRUE),
-            .groups = "drop") |>
-  mutate(submit_rate = head_total/catch_estimate_predicted)
-
-
-ggplot(region_head_df, aes(x=catch_estimate_predicted, y=head_total)) +
-  geom_point() +
-  geom_smooth(method='lm', formula= y~x) +
-  geom_text(x = 500, y = 5000, label = lm_eqn(region_head_df), parse = TRUE, hjust=0, vjust=0)
-
-
-ggplot(region_head_df, aes(x=YEAR, y=submit_rate, color=region_name)) +
-  geom_line(size = 3)
+#head_df <-
+#  LoadHeadTotals() |>
+#  filter(!is.na(region))
+#
+#
+#head_catch_df <-
+#  models_combined |>
+#  filter(status == "marked_Kept_total") |>
+#  full_join(head_df, c(YEAR="year", finescale_fishery = "region")) |>
+#  mutate(YEAR = as.integer(YEAR),
+#         submit_rate = head_total/catch_estimate_predicted)
+#
+#
+#head_catch_model_df <-
+#  head_catch_df |>
+#  filter(catch_estimate_predicted > 0, !(grepl("NBC", head_catch_df$finescale_fishery) & YEAR < 2015))
+#
+#head_model <- lm(head_total ~ catch_estimate_predicted + 0, data=head_catch_model_df)
+#
+#
+#
+#
+#ggplot(data = head_catch_model_df, aes(x=catch_estimate_predicted,
+#                                       y=head_total)) +
+#  geom_point(aes(color = finescale_fishery),
+#             size=2) +
+#  geom_smooth(method='lm', formula= y~x) +
+#  geom_text(x = 500, y = 5000, label = lm_eqn(head_catch_model_df), parse = TRUE, hjust=0, vjust=0)
+#
+#head_catch_model_df |>
+#  filter(catch_estimate_predicted > 20) |>
+#  ggplot(aes(x=YEAR, y=submit_rate, color=finescale_fishery)) +
+#  geom_line(size = 3)
+#
+#region_head_df <-
+#  head_catch_model_df |>
+#  mutate(region_name = substring(finescale_fishery,1,6)) |>
+#  mutate(region_name = paste0(region_name,
+#                              if_else(grepl("ISBM", finescale_fishery), "ISBM", ""))) |>
+#  mutate(region_name = trimws(region_name)) |>
+#  group_by(YEAR, region_name) |>
+#  summarize(head_total = sum(head_total, na.rm=TRUE),
+#            catch_estimate_predicted = sum(catch_estimate_predicted, na.rm=TRUE),
+#            .groups = "drop") |>
+#  mutate(submit_rate = head_total/catch_estimate_predicted)
+#
+#
+#ggplot(region_head_df, aes(x=catch_estimate_predicted, y=head_total)) +
+#  geom_point() +
+#  geom_smooth(method='lm', formula= y~x + 0) +
+#  geom_text(x = 500, y = 5000, label = lm_eqn(region_head_df), parse = TRUE, hjust=0, vjust=0)
+#
+#
+#ggplot(region_head_df, aes(x=YEAR, y=submit_rate, color=region_name)) +
+#  geom_line(size = 3)
+#
